2309ab0c3199d766e855763f120bd57ec7d8ecc6
MarcosZHU <101159150+MarcosZHU@users.noreply.github.com>
2022-03-08 11:03:09 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '74e0fa4d13a84ee38c1e8598b2962358',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Mr. Space',
+  domain: 'https://kehengzhu.notion.site/Kecheng-ZHU-s-Blog-74e0fa4d13a84ee38c1e8598b2962358',
+  author: 'Marcos ZHU',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  wechat: 'syzkc000106',
+  github: 'MarcosZHU',
+  behance: 'Kecheng ZHU',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
